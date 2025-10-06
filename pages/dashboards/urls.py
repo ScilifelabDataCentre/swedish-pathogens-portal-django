18718cc4b3f6a05542736ee0913627bf101d7931
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2025-10-06 16:13:05 +0200

    FREYA-1680: Add SARS-CoV-2 Variant Competition dashboard (#24)
    * feat(dashboard): add new dashboard template
    
    * feat(dashboard): add new view
    
    * feat(dashboard): add new url path
    
    * feat(dashboard): update dashboards index
    
    * feat(accessibility): Add skip links + formatting

--- pages/dashboards/urls.py
+++ pages/dashboards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardsIndex
+from .views import DashboardsIndex, LineageCompetition
 
 app_name = "dashboards"
 
 urlpatterns = [
     path("", DashboardsIndex.as_view(), name="index"),
+    path("lineage-competition/", LineageCompetition.as_view(), name="lineage_competition"),
 ]
--- pages/dashboards/views.py
+++ pages/dashboards/views.py
@@ -9,3 +9,14 @@ class DashboardsIndex(BaseTemplateView):
 
     template_name = "dashboards/index.html"
     title = "Data dashboards"
+
+
+class LineageCompetition(BaseTemplateView):
+    """SARS-CoV-2 Variant Competition dashboard page."""
+
+    template_name = "dashboards/lineage_competition.html"
+    title = "SARS-CoV-2 Variant Competition"
+    description = (
+        "Estimates of SARS-CoV-2 variant frequencies and growth rate advantages from "
+        "global SARS-CoV-2 genotype sequencing data"
+    )
