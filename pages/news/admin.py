5e010401a3d8c93dfb58f484fa5d1f8d7e1465a7
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2025-11-05 21:56:00 +0530

    Make created_at date editable

--- pages/news/admin.py
+++ pages/news/admin.py
@@ -21,7 +21,7 @@ class NewsAdmin(admin.ModelAdmin):
     list_filter = ["is_active", "created_at"]
     search_fields = ["title", "summary"]
     prepopulated_fields = {"slug": ("title",)}
-    readonly_fields = ["created_at", "updated_at"]
+    readonly_fields = ["updated_at"]
     date_hierarchy = "created_at"
     ordering = ["-created_at"]
 
