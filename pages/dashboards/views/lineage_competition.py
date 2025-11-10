d867ee17077005322034230f1a2732f6fccfa2f4
Senthilkumar Panneerselvam <5311007+senthil10@users.noreply.github.com>
2025-11-10 22:52:43 +0530

    FREYA-1812: Split dashbaord views.py and initialise visualisation folder (#33)
    * Split views.py to views directory
    
    * Initialise visualisation folder to house parsing scripts

---
+++ pages/dashboards/views/lineage_competition.py
@@ -0,0 +1,21 @@
+from utils.views import BaseTemplateView
+
+
+class LineageCompetition(BaseTemplateView):
+    """SARS-CoV-2 Variant Competition dashboard page.
+
+    This dashboard view class renders the given template in which the plots are
+    directly fetched from researchers Github repository.
+
+    Attributes:
+        template_name: Template for rendering the dashboard.
+        title: Title displayed in the rendered page's banner section.
+        description: Description to be used in the HTML's head.
+    """
+
+    template_name = "dashboards/lineage_competition.html"
+    title = "SARS-CoV-2 Variant Competition"
+    description = (
+        "Estimates of SARS-CoV-2 variant frequencies and growth rate advantages from "
+        "global SARS-CoV-2 genotype sequencing data"
+    )
