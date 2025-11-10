011e6a64634c374e4be7ead3533ceee72d6d74eb
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2025-11-10 19:59:36 +0530

    Initialise visualition folder to house parsing scripts

---
+++ pages/dashboards/visualisation/__init__.py
@@ -0,0 +1,10 @@
+"""Visualisation scripts for the dashboards app.
+
+This folder i.e. dashboards/visualisation will have the
+scripts/functions/classes to parse, handle and update
+the required graph/table/relevant data by parsing the
+raw data for each active dashboards.
+
+Re-exports the needed scripts/functions/classes to provide
+a stable and neat import.
+"""
