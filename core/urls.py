699b39661107ec4622217321b6f579d7aa3e97c1
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-14 09:32:37 +0100

    move healthz url to fit in alphabetical order in urls

--- core/urls.py
+++ core/urls.py
@@ -38,13 +38,13 @@ def healthz(_request):
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls, name="admin"),
-    path("healthz/", healthz, name="healthz"),
     path("", include("pages.home.urls")),
     path("articles/", include("pages.articles.urls")),
     path("about/", include("pages.about.urls")),
     path("citation/", include("pages.citation.urls")),
     path("dashboards/", include("pages.dashboards.urls")),
     path("data-management/", include("pages.data_management.urls")),
+    path("healthz/", healthz, name="healthz"),
     path("news/", include("pages.news.urls")),
     path("privacy/", include("pages.privacy.urls")),
     path("topics/", include("pages.topics.urls")),
