a7d6291d4797581da7de9cbcb2767567b4f77085
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:25:10 +0100

    docstring and verbose name in dashboards app

--- pages/dashboards/apps.py
+++ pages/dashboards/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class DashboardsConfig(AppConfig):
+    """Configuration for the dashboards app.
+    
+    This app provides interactive dashboards for visualizing and exploring
+    pathogen-related data on the Swedish Pathogens Portal. It enables users to
+    gain insights through dynamic charts, graphs, and maps.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.dashboards"
+    verbose_name = "Dashboards"
