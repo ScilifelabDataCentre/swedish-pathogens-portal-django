b26d6136b2cc0e586e79a42e1a607e2d558e95ef
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:32:05 +0100

    placeholder for docstring

--- pages/data_management/app.py
+++ pages/data_management/app.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 
 class DataManagementConfig(AppConfig):
+    """Configuration for the data management app.
+    
+    TODO: Some description about the app can go here.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages.data_management'
     verbose_name = "Data Management"
