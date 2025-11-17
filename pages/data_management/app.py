cf352164ad68f0ad7ec930b74e7695941cbb7daa
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:29:35 +0100

    verbose name for data_management

--- pages/data_management/app.py
+++ pages/data_management/app.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DataManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages.data_management'
+    verbose_name = "Data Management"
