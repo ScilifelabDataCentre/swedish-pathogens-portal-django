ef6ecfaa8a7c6a10681bb2ac10d43035e3ccb906
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:19:03 +0100

    edit docstring

--- pages/about/apps.py
+++ pages/about/apps.py
@@ -4,8 +4,7 @@ from django.apps import AppConfig
 class AboutConfig(AppConfig):
     """Configuration for the about app.
     
-    This app provides information about the Swedish Pathogens Portal,
-    including its mission, team, and contact details.
+    This app provides information about the Swedish Pathogens Portal.
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.about"
