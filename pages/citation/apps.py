4139afe80c1f8964da877ee83564fa44c0f36e7f
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:15:50 +0100

    docstring and verbose name to citation app

--- pages/citation/apps.py
+++ pages/citation/apps.py
@@ -2,7 +2,12 @@ from django.apps import AppConfig
 
 
 class CitationConfig(AppConfig):
+    """Configuration for the citation app.
+    
+    This app handles citation information and guidelines for the Swedish
+    Pathogens Portal, ensuring proper acknowledgment of the portal in academic
+    and research publications."""
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.citation"
-
+    verbose_name = "Citation"
 
