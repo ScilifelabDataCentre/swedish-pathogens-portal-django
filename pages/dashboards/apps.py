f09f80f8543777ad40393780b10bbc5527dc35ea
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:28:16 +0100

    edit docstring for dashboards app

--- pages/dashboards/apps.py
+++ pages/dashboards/apps.py
@@ -4,9 +4,7 @@ from django.apps import AppConfig
 class DashboardsConfig(AppConfig):
     """Configuration for the dashboards app.
     
-    This app provides interactive dashboards for visualizing and exploring
-    pathogen-related data on the Swedish Pathogens Portal. It enables users to
-    gain insights through dynamic charts, graphs, and maps.
+    This app provides dashboards for visualizing and analyzing pathogen-related data.
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.dashboards"
