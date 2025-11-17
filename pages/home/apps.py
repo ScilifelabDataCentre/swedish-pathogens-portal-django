0655f8c3a0eda21d5ec85612e6dc398e0e89b463
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-17 09:33:42 +0100

    docstring for home app

--- pages/home/apps.py
+++ pages/home/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    """Configuration for the home app.
+    
+    This app serves as the home page for the Swedish Pathogens Portal.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.home"
     verbose_name = "Home"
