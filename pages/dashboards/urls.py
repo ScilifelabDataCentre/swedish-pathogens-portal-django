86500aa506082e5f06a0bf5d10db5354efeddbd5
Senthilkumar Panneerselvam <5311007+senthil10@users.noreply.github.com>
2025-11-18 18:33:16 +0530

    FREYA-1692: Serology statistics dashboard (#38)
    * Add plotly template (temporary)
    
    * Add serology statistics template
    
    * Add serology statistics view
    
    * Add temporary ref to dashboard index
    
    * Fetch plot data on server side

--- pages/dashboards/urls.py
+++ pages/dashboards/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import DashboardsIndex, LineageCompetition
+from .views import DashboardsIndex, LineageCompetition, SerologyStatistics
 
 app_name = "dashboards"
 
 urlpatterns = [
     path("", DashboardsIndex.as_view(), name="index"),
-    path("lineage-competition/", LineageCompetition.as_view(), name="lineage_competition"),
+    path(
+        "lineage-competition/", LineageCompetition.as_view(), name="lineage_competition"
+    ),
+    path(
+        "serology-statistics/", SerologyStatistics.as_view(), name="serology_statistics"
+    ),
 ]
