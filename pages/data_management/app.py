130eed7915cf5b4b5fd15c854e39a215301ddc64
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-18 08:50:21 +0100

    data management docstring description

--- pages/data_management/app.py
+++ pages/data_management/app.py
@@ -4,7 +4,8 @@ from django.apps import AppConfig
 class DataManagementConfig(AppConfig):
     """Configuration for the data management app.
     
-    TODO: Some description about the app can go here.
+    This app handles the research data management (RDM) page, 
+    which provides advice and links to RDM-relevant resources.
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages.data_management'
