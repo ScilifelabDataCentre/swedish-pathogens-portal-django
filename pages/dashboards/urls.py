4ac90bdeddf3d0efacfcfdf7f159eafcf4963d60
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2025-11-18 20:52:29 +0100

    fix(dashboards): Resolve merge conflict in URLs and ensure proper import of views

--- pages/dashboards/urls.py
+++ pages/dashboards/urls.py
@@ -1,23 +1,15 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import DashboardsIndex, LineageCompetition, VariantsRegionUppsala
-=======
-from .views import DashboardsIndex, LineageCompetition, SerologyStatistics
->>>>>>> origin/main
+from .views import DashboardsIndex, LineageCompetition, SerologyStatistics, VariantsRegionUppsala
 
 app_name = "dashboards"
 
 urlpatterns = [
     path("", DashboardsIndex.as_view(), name="index"),
-<<<<<<< HEAD
-    path("lineage-competition/", LineageCompetition.as_view(), name="lineage_competition"),
-    path("variants-region-uppsala/", VariantsRegionUppsala.as_view(), name="variants_region_uppsala"),
-=======
     path(
         "lineage-competition/", LineageCompetition.as_view(), name="lineage_competition"
     ),
     path(
         "serology-statistics/", SerologyStatistics.as_view(), name="serology_statistics"
     ),
->>>>>>> origin/main
+    path("variants-region-uppsala/", VariantsRegionUppsala.as_view(), name="variants_region_uppsala"),
 ]
