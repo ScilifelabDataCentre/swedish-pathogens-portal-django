5d17df697efbd7c7def3471eca7eb51e1b256e1c
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-18 08:52:14 +0100

    privacy docstring description

--- pages/privacy/apps.py
+++ pages/privacy/apps.py
@@ -4,7 +4,9 @@ from django.apps import AppConfig
 class PrivacyConfig(AppConfig):
     """Configuration for the privacy app.
     
-    TODO: Add short description of privacy app here.
+    This app handles the privacy page, which informs users 
+    about what data is collected about them when using the site, 
+    and how it is handled.
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.privacy"
