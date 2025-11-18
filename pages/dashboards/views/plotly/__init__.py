dbeb9937f2ecd79c032eefed570ca444714617b1
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2025-11-18 20:25:38 +0100

    refactor(dashboards): Remove Plotly abstraction

--- pages/dashboards/views/plotly/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Plotly visualization views package for dashboard pages.
-
-This package provides view classes for dashboard pages that need to fetch
-and render Plotly visualizations from external data sources.
-"""
-
-from .plotly import PlotlyDashboardView
-
-__all__ = ["PlotlyDashboardView"]
