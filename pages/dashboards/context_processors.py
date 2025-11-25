bb3b2cfd43b4b5dd257879bf01049b1a7473d09e
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-11-25 08:53:23 +0100

    Reapply "Merge branch 'main' into yamllint"
    This reverts commit 802dd129ac845173de4fa279dbe415f6138d99f0.

---
+++ pages/dashboards/context_processors.py
@@ -0,0 +1,11 @@
+from django.http import HttpRequest
+
+from pages.dashboards.visualisation.utils import get_plotlyjs_cdn_param
+
+
+def plotlyjs(request: HttpRequest) -> dict:
+    """Make plotly JS url and hash available in all template's context by default"""
+    return {
+        "plotlyjs_url": get_plotlyjs_cdn_param("url"),
+        "plotlyjs_hash": get_plotlyjs_cdn_param("hash"),
+    }
