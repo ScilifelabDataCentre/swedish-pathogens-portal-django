f6c2d16187c64c53d07aef4d95135ba0fc090fd9
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2025-11-26 15:16:03 +0100

    FREYA-1700: Add NPC Statistics dashboard (#45)
    * feat(dashboards): Add NPC statistics dashboard view
    
    * feat(dashboards): Add NPC statistics HTML template
    
    * feat(dashboards): Add NPC statistics view and update URL configuration
    
    * feat(dashboards): Add link to NPC COVID-19 test statistics in dashboard index
    
    * refactor(dashboards): Remove NPC COVID-19 test statistics link from dashboard index
    
    * feat(dashboards): Include Plotly.js in NPC statistics template

--- pages/dashboards/views/__init__.py
+++ pages/dashboards/views/__init__.py
@@ -6,6 +6,7 @@ URL configs and other modules.
 
 from .index import DashboardsIndex
 from .lineage_competition import LineageCompetition
+from .npc_statistics import NpcStatistics
 from .post_covid import PostCovid
 from .serology_statistics import SerologyStatistics
 from .symptom_study_sweden import SymptomStudySweden
@@ -15,6 +16,7 @@ from .variants_region_uppsala import VariantsRegionUppsala
 __all__ = [
     "DashboardsIndex",
     "LineageCompetition",
+    "NpcStatistics",
     "PostCovid",
     "SerologyStatistics",
     "SymptomStudySweden",
---
+++ pages/dashboards/views/npc_statistics.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.views import View
+
+from ..visualisation.utils import fetch_plot_json_blobserver, plot_html_from_json
+
+
+class NpcStatistics(View):
+    """National Pandemic Centre SARS-CoV-2 test statistics dashboard page.
+
+    Displays statistics and visualizations of SARS-CoV-2 (COVID-19) tests
+    conducted at the National Pandemic Centre (NPC) at Karolinska Institute.
+    This dashboard is historic and no longer updated.
+
+    Attributes:
+        template_name: Template for rendering the dashboard.
+        title: Title displayed in the rendered page's banner section.
+        description: Description to be used in the HTML's head.
+    """
+
+    template_name = "dashboards/npc_statistics.html"
+    title = "National Pandemic Centre SARS-CoV-2 (COVID-19) test statistics"
+    description = (
+        "The national Pandemic Centre (NPC) conducted testing related to "
+        "SARS-CoV-2 from the start of the pandemic. They show positive, "
+        "negative, and inconclusive tests. This dashboard is historic, so "
+        "no longer updated."
+    )
+
+    def get(self, request):
+        """Fetch the compiled plot data (JSON) and generate plot HTML strings.
+
+        Fetches Plotly JSON data from blobserver for each chart, converts it to HTML
+        using plot_html_from_json, and adds it to the context for template rendering.
+
+        Returns:
+            Rendered template with plot HTML strings in context.
+        """
+        context = dict(title=self.title, description=self.description)
+
+        # Map chart IDs to blobserver blob names
+        plot_sources = {
+            "total_tests": "npc_total_tests.json",
+            "tests_daily": "npc_tests_daily.json",
+            "tests_weekly": "npc_tests_weekly.json",
+            "positive_fraction_daily": "npc_positiveTests_fraction_daily.json",
+            "positive_fraction_weekly": "npc_positiveTests_fraction_weekly.json",
+            "cumulative_tests": "npc_cumulative_tests.json",
+        }
+
+        # Fetch and convert each plot
+        for chart_id, blob_name in plot_sources.items():
+            blob_data = fetch_plot_json_blobserver(blob_name)
+            if blob_data is not None:
+                # Set height based on chart type
+                if chart_id == "total_tests":
+                    height = "200px"
+                elif chart_id == "cumulative_tests":
+                    height = "550px"
+                else:
+                    height = "350px"
+                context[chart_id] = plot_html_from_json(
+                    blob_data,
+                    height=height,
+                    skip_invalid=True,
+                )
+
+        return render(request, self.template_name, context)
