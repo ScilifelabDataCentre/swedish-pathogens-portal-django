4963ee585a254b1caa5440bdef7cad0123f4ba04
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-01 10:13:36 +0100

    citation views docstring

--- pages/citation/views.py
+++ pages/citation/views.py
@@ -1,6 +1,10 @@
+"""Views for the Citation page."""
+
 from utils.views import BaseTemplateView
 
 
 class Citation(BaseTemplateView):
+    """A template view class to render Citation page."""
+
     template_name = "citation/index.html"
     title = "How to cite the Portal"
