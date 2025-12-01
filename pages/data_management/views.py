350d72e848fe38202c613fccc27f7cd7ad03df56
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-01 10:43:45 +0100

    data management url docstring and docstring for class

--- pages/data_management/views.py
+++ pages/data_management/views.py
@@ -1,6 +1,10 @@
+"""Views for the data management page."""
+
 from utils.views import BaseTemplateView
 
 
 class DataManagement(BaseTemplateView):
+    """A template view class to render Data Management page."""
+
     template_name = "data_management/index.html"
     title = "Research Data Management"
