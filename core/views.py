8ceb0b011e46b77687ed69efa744dc91ff37ada6
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-01 09:16:49 +0100

    _request type

--- core/views.py
+++ core/views.py
@@ -3,10 +3,10 @@
 Endpoints defined here are used across the system.
 """
 
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 
 
-def healthz(_request):
+def healthz(_request: HttpRequest) -> JsonResponse:
     """Health check endpoint.
 
     Used for monitoring uptime of the service.
