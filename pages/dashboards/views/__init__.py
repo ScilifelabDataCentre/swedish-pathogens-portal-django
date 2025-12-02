c3a2eb2f684b8f9e7b7fab42ba17c276366e35e2
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2025-12-02 17:41:38 +0100

    Make new dashboard view available to the app

--- pages/dashboards/views/__init__.py
+++ pages/dashboards/views/__init__.py
@@ -4,8 +4,9 @@ Re-exports view classes to provide a stable import surface for
 URL configs and other modules.
 """
 
-from .crush_covid import CrushCovid
 from .covid_quantification_kth import CovidQuantificationKth
+from .crush_covid import CrushCovid
+from .historic_sarscov2_wastewater import HistoricSarsCov2Wastewater
 from .index import DashboardsIndex
 from .lineage_competition import LineageCompetition
 from .npc_statistics import NpcStatistics
@@ -16,9 +17,10 @@ from .vaccines import Vaccines
 from .variants_region_uppsala import VariantsRegionUppsala
 
 __all__ = [
-    "CrushCovid",
     "CovidQuantificationKth",
+    "CrushCovid",
     "DashboardsIndex",
+    "HistoricSarsCov2Wastewater",
     "LineageCompetition",
     "NpcStatistics",
     "PostCovid",
