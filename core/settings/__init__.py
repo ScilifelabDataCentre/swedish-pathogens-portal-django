d9a0ea0ac859cf4d86609f20099b57669a2814a1
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-16 10:40:20 +0100

    docstring in init

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -5,4 +5,5 @@ The settings are split into three files:
 - base.py: Base settings shared by all environments
 - development.py: Development settings for local development
 - production.py: Production settings for deployments
+- testing.py: Settings specific to testing environment
 """
