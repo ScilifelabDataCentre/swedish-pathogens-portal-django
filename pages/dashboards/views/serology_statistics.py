26ca0fab61f6fcacd47e3817c9496950c291cfc3
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-16 11:54:26 +0100

    revert space

--- pages/dashboards/views/serology_statistics.py
+++ pages/dashboards/views/serology_statistics.py
@@ -17,7 +17,6 @@ class SerologyStatistics(View):
         template_name: Template for rendering the dashboard.
         title: Title displayed in the rendered page's banner section.
         description: Description to be used in the HTML's head.
-
     """
 
     template_name = "dashboards/serology_statistics.html"
