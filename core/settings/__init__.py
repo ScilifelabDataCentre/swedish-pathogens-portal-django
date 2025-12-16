b121ff90c0cda61c9abed238a87f752064d506f7
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-12-16 10:40:49 +0100

    four

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,6 +1,6 @@
 """Settings package for Pathogens Portal.
 
-The settings are split into three files:
+The settings are split into four files:
 
 - base.py: Base settings shared by all environments
 - development.py: Development settings for local development
