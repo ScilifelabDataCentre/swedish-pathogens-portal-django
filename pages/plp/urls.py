86abd042813a679c2fb78fa7001c468daacfe0fb
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2025-12-17 18:11:47 +0100

    Create the PlpProjectDetail view for PLP projects

--- pages/plp/urls.py
+++ pages/plp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PlpProgram
+from .views import PlpProgram, PlpProjectDetail
 
 app_name = "plp"
 
 urlpatterns = [
     path("", PlpProgram.as_view(), name="index"),
+    path("projects/<slug:slug>/", PlpProjectDetail.as_view(), name="project-detail"),
 ]
--- pages/plp/views.py
+++ pages/plp/views.py
@@ -1,6 +1,19 @@
-from utils.views import BaseTemplateView
+from utils.views import BaseTemplateView, BaseDetailView
+
+from .models import PlpProject
 
 
 class PlpProgram(BaseTemplateView):
     template_name = "plp/index.html"
     title = "Pandemic Laboratory Preparedness Program"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["projects"] = PlpProject.objects.filter(is_active=True).order_by("-created_at")
+        return context
+
+
+class PlpProjectDetail(BaseDetailView):
+    template_name = "plp/project_detail.html"
+    model = PlpProject
+    context_object_name = "project"
