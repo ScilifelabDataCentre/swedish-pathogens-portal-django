de3e211a36b17402a80a722207002b5fbc9ee7b6
Rickard Hammarén <rickard.hammaren@scilifelab.uu.se>
2026-01-09 13:51:52 +0100

    linting fixes to services

--- pages/portal_data/apps.py
+++ pages/portal_data/apps.py
@@ -1,6 +1,9 @@
+"""App configuration for the portal_data app."""
 from django.apps import AppConfig
 
 
 class PortalDataConfig(AppConfig):
+    """Configuration for the portal_data application."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages.portal_data"
