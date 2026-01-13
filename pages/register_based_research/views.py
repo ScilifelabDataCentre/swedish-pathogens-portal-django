4ce58fd458521926c48a2b9c21de214d5a5636f4
HamsaiyniVenkatesh <nalina.venkatesh@umu.se>
2026-01-13 12:54:15 +0100

    Added missing docstring

--- pages/register_based_research/views.py
+++ pages/register_based_research/views.py
@@ -4,5 +4,7 @@ from utils.views import BaseTemplateView
 
 
 class RegisterBasedResearch(BaseTemplateView):
+    """A template view class to render Register Based Research page."""
+
     template_name = "register_based_research/index.html"
     title = "Register Based Research"
