1635c7d613360c992706ee3c6ec792f800810eae
Ina Odén Österbo <i-oden@users.noreply.github.com>
2026-01-26 12:38:39 +0100

    change comments

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -26,9 +26,11 @@ def _iter_static_named_urls(
         str: Fully namespaced URL name for each static named URL found (e.g. "about:index").
     """
     for entry in url_patterns:
-        if isinstance(entry, URLResolver):  # example of entry: admin, home, about, etc.
+        # Top-level URL configs will resolve true, e.g. about, articles, etc.
+        if isinstance(entry, URLResolver):
             # Get all namespaces e.g. about:index/partners/funders/nodes
-            child_namespace = entry.namespace or namespace  # single-level namespaces so far
+            # Single-level namespaces only for now
+            child_namespace = entry.namespace or namespace
             yield from _iter_static_named_urls(
                 url_patterns=entry.url_patterns, namespace=child_namespace
             )
