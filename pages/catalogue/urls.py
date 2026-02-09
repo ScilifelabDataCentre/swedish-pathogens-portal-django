a51113b8449fc973a94196daafe553c8ed4efc87
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2026-02-09 17:30:10 +0100

    Add a `RedirectView` from app root to homepage

--- pages/catalogue/urls.py
+++ pages/catalogue/urls.py
@@ -1,12 +1,14 @@
 """URLs for catalogue pages."""
 
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 app_name = "catalogue"
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="home:index")),
     path("tools-catalogue/", views.ToolsCatalogueListView.as_view(), name="tools_catalogue"),
     path("data-repositories/", views.DataRepositoriesListView.as_view(), name="data_repositories"),
     path("data-sources/", views.DataSourcesListView.as_view(), name="data_sources"),
