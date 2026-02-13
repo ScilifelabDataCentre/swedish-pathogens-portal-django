d2007fda9f0108d600c9f4b2da4ed60a045e4568
Senthilkumar Panneerselvam <5311007+senthil10@users.noreply.github.com>
2026-02-13 11:51:05 +0100

    Add default handler for media files

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,7 +6,7 @@ These settings are intended for deployments.
 from .base import *  # noqa: F401,F403
 from .base import env
 
-DEBUG = True
+DEBUG = False
 
 # ADMIN
 # ------------------------------------------------------------------------------
@@ -14,14 +14,32 @@ ADMIN_URL = env("ADMIN_URL").rstrip("/") + "/"
 # ADMINS = [(Full name, email address)]
 # MANAGERS = ADMINS
 
+
+# MEDIA FILES (Production)
+# ------------------------------------------------------------------------------
+MEDIA_ROOT = env("MEDIA_ROOT")
+MEDIA_URL = env("MEDIA_URL", default="media").rstrip("/") + "/"
+
+
 # PRODUCTION STATIC FILE SETTINGS
 # ------------------------------------------------------------------------------
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 STORAGES = {
+    # Default storage for uploaded files (media)
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": MEDIA_ROOT,  # points to writable PVC
+        },
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "OPTIONS": {
+            "location": STATIC_ROOT,  # noqa: F405 (import from base.py)
+        },
     },
 }
+
 
 # SECURITY
 # ------------------------------------------------------------------------------
@@ -39,12 +57,6 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=0)
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("SECURE_HSTS_INCLUDE_SUBDOMAINS", default=False)
 SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=False)
-
-
-# MEDIA FILES (Production)
-# ------------------------------------------------------------------------------
-MEDIA_ROOT = env("MEDIA_ROOT")
-MEDIA_URL = env("MEDIA_URL", default="media").rstrip("/") + "/"
 
 
 # EMAIL (Production via env; placeholders acceptable)
